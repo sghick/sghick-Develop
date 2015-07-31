920292b4297c0b04ec850f364f16fbdc4b42100f
丁治文 <sghick@sina.com>
2015-07-31 11:14:29 +0800

    new SMFrameWork

---
+++ Project/App/SMFrameWork/SMFrameWork/SMFrameWork.h
@@ -0,0 +1,13 @@
+//
+//  SMFrameWork.h
+//  SMFrameWork
+//
+//  Created by buding on 15/7/31.
+//  Copyright (c) 2015年 buding. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SMFrameWork : NSObject
+
+@end
---
+++ Project/App/SMFrameWork/SMFrameWork/SMFrameWork.m
@@ -0,0 +1,13 @@
+//
+//  SMFrameWork.m
+//  SMFrameWork
+//
+//  Created by buding on 15/7/31.
+//  Copyright (c) 2015年 buding. All rights reserved.
+//
+
+#import "SMFrameWork.h"
+
+@implementation SMFrameWork
+
+@end
