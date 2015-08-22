d0feb7622d81ba444099b6a7d26a834b4200e371
丁治文 <sghick@Mac-mini.local>
2015-08-22 12:56:20 +0800

    增加一个测试view mark不能完美支持旋转

--- Other/SMAttributies/SMAttributies/ViewController.m
+++ Other/SMAttributies/SMAttributies/ViewController.m
@@ -25,6 +25,10 @@
     UIView *view2 = [[UIView alloc] init];
     view2.backgroundColor = [UIColor colorWithRed:122/255.0 green:222/255.0 blue:20/255.0 alpha:0.5];
     [view1 addSubview:view2 attributePathKey:@"ViewController.frame2"];
+    
+    UIView *view3 = [[UIView alloc] init];
+    view3.backgroundColor = [UIColor colorWithRed:10/255.0 green:10/255.0 blue:220/255.0 alpha:0.5];
+    [view2 addSubview:view3 attributePathKey:@"ViewController.frame3"];
 }
 
 @end
